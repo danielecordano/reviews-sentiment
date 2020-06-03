4ef9f83e7b33bf89d88fc81e8609c1418115c670
Daniele Cordano <daniele.cordano@gmail.com>
2020-06-03 14:22:48 +0200

    It works!

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,8 @@
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
-admin.initializeApp();
+admin.initializeApp({
+  credential: admin.credential.applicationDefault(),
+});
 const { google } = require("googleapis");
 const ml = google.ml("v1");
 
